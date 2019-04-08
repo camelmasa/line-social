d1d0472c92ccea80222b2576a4f140d064245565
camelmasa <camelmasa@gmail.com>
2019-04-09 00:02:53 +0900

    refactor: tiny

--- lib/line/social/request/base.rb
+++ lib/line/social/request/base.rb
@@ -2,8 +2,8 @@ module Line
   module Social
     module Request
       class Base
-        def request 
-          Faraday.new(url: "#{url}") do |connection|
+        def request
+          Faraday.new(url: url) do |connection|
             connection.response :json, content_type: /\bjson$/
             connection.adapter Faraday.default_adapter
           end
